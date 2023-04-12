f8b279555f1e1856e451c7b01c1853f2f643d537
toriumi <syouei72@icloud.com>
2023-04-13 00:37:15 +0900

    個別ページ型だけ整えた

--- pages/buyAndSell/[item_id].vue
+++ pages/buyAndSell/[item_id].vue
@@ -1,14 +1,66 @@
 <script setup lang="ts">
+import goodsArr from '~/goodsList.json'
+
 const route = useRoute()
+const goodsList: any = ref(goodsArr.arr)
 const itemId = ref(route.params.item_id)
+const item = ref(goodsList.value.find((goodsItem: any) => goodsItem.id == itemId.value))
+
+onMounted(() => {
+  
+})
+
+const onGoBack = () => {
+  window.history.back()
+}
 </script>
 
 <template>
-  <div>
-    {{ itemId }}
-  </div>
+  <ClientOnly>
+    <div class="h-screen">
+      <!-- 商品画像 -->
+      <div class="h-1/2 w-full">
+        <img class="w-full h-full object-cover" :src="item.image" :alt="item.name" />
+      </div>
+
+      <!-- 閉じるボタン -->
+      <button
+        @click="onGoBack"
+        class="absolute top-4 right-4 p-2 bg-white rounded-full shadow-md"
+      >
+        &times;
+      </button>
+
+      <!-- 商品詳細 -->
+      <div class="h-1/2 p-4 flex flex-col">
+        <h1 class="text-2xl font-semibold mb-4">{{ item.name }}</h1>
+        <div class="mb-4 flex flex-wrap">
+          <span
+            v-for="(tag, index) in item.tags"
+            :key="index"
+            class="mr-2 mb-2 px-3 py-1 bg-gray-300 rounded-full"
+          >
+            {{ tag }}
+          </span>
+        </div>
+        <div class="mb-4">
+          <span class="text-lg font-semibold">{{ item.price }}円</span>
+        </div>
+        <div class="mb-4">出版年: {{ item.publishedYear }}</div>
+        <div class="mb-4">{{ item.description }}</div>
+
+        <!-- 取引へのボタン -->
+        <div class="mt-auto">
+          <button
+            class="w-full py-2 bg-blue-500 text-white rounded-md shadow-md"
+          >
+            取引へ
+          </button>
+        </div>
+      </div>
+    </div>
+  </ClientOnly>
 </template>
 
 <style scoped>
-
 </style>
--- pages/buyAndSell/index.vue
+++ pages/buyAndSell/index.vue
@@ -4,8 +4,9 @@ import goodsArr from '~/goodsList.json';
 
 register();
 
-const goodsList:any = ref(goodsArr);
+const goodsList:any = ref(goodsArr.arr);
 const tabArr = ref([
+  "全て",
   "プログラミング",
   "物理",
   "数学",
@@ -32,79 +33,71 @@ const focusInput = (event:any) => {
   const inputElement = event.target;
   inputElement.focus();
   inputElement.setSelectionRange(inputElement.value.length, inputElement.value.length);
-};
+}
 
+//scroll位置の保持(シームレスにできた気がするから後で確認)
 
-// const onProgress = (e:any) => {
-  // const [swiper, progress] = e.detail;
-// }
-
+//swiper
 const onSlideTo = (e:any) => {
   e.detail[0].slideTo(e.detail[0].clickedIndex - 1, 500, true)
 }
-
 </script>
 
 <template>
-  <ClientOnly>
-    <div class="">
-      <!-- thumbs swiper -->
-      <swiper-container
-        ref="tabs"
-        class="tabs sticky top-0 pt-3 px-4 bg-white shadow-[0px_5px_6px_-1px_rgba(0,0,0,0.03)] z-[50]"
-        :slides-per-view="`auto`"
-        :space-between="30"
-        :watchSlidesVisibility="true"
-        :watchSlidesProgress="true"
-        :slideActiveClass="'swiper-slide-active'"
-        @tap="onSlideTo($event)"
-      >
-        <swiper-slide class="w-auto opacity-30">
-          <div class="w-auto">
-            <input
-              @click="focusInput($event)"
-              v-model="searchName"
-              placeholder="search"
-              :size="searchNameLength"
-              class="w-auto outline-none whitespace-nowrap overflow-hidden overflow-ellipsis text-center"
-            />
-          </div>
-          <div class="line w-full h-1 mt-2 bg-[#644DAC] rounded-full opacity-0"></div>
-        </swiper-slide>
-        <swiper-slide v-for="(tab, index) of tabArr" class="w-auto opacity-30">
-          <div class="">{{ tab }}</div>
-          <div class="line w-full h-1 mt-2 bg-[#644DAC] rounded-full opacity-0"></div>
-        </swiper-slide>
-      </swiper-container>
-
-      <!-- contents swiper -->
-      <swiper-container
-        ref="content"
-        thumbs-swiper=".tabs"
-        @slidechange=""
-        class=""
-      >
-        <swiper-slide>{{ searchName }}</swiper-slide>
-        <swiper-slide v-for="(tab, index) of tabArr">
-          <div class="pt-3">
-            <ListGoods class="mx-auto container px-2 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16">
-              <button v-for="goodsItem of goodsList">
-                <ItemGoods :goodsItem="goodsItem" />
-              </button>
-            </ListGoods>
-          </div>
-        </swiper-slide>
-      </swiper-container>
-    </div>
-  </ClientOnly>
+  <div class="">
+    <!-- thumbs swiper -->
+    <swiper-container 
+      ref="tabs"
+      class="tabs sticky top-0 pt-3 px-4 bg-white shadow-[0px_5px_6px_-1px_rgba(0,0,0,0.03)] z-[50]"
+      :slides-per-view="`auto`" 
+      :space-between="30" 
+      :watchSlidesVisibility="true"
+      :watchSlidesProgress="true" 
+      :slideActiveClass="'swiper-slide-active'" 
+      @tap="onSlideTo($event)"
+    >
+      <swiper-slide class="w-auto opacity-30">
+        <div class="w-auto">
+          <input
+            @click="focusInput($event)"
+            v-model="searchName"
+            placeholder="search"
+            :size="searchNameLength"
+            class="w-auto outline-none whitespace-nowrap overflow-hidden overflow-ellipsis text-center"
+          />
+        </div>
+        <div class="line w-full h-1 mt-2 bg-[#644DAC] rounded-full opacity-0"></div>
+      </swiper-slide>
+      <swiper-slide v-for="(tab, index) of tabArr" class="w-auto opacity-30">
+        <div class="">{{ tab }}</div>
+        <div class="line w-full h-1 mt-2 bg-[#644DAC] rounded-full opacity-0"></div>
+      </swiper-slide>
+    </swiper-container>
+  
+    <!-- contents swiper -->
+    <swiper-container 
+      ref="content"
+      thumbs-swiper=".tabs"
+      @slidechange=""
+      class="bg-[#EFF0F0]"
+    >
+      <swiper-slide>{{ searchName }}</swiper-slide>
+      <swiper-slide v-for="(tab, index) of tabArr">
+        <ListGoods class="mx-auto container px-2 sm:px-6 md:px-8 lg:px-10 xl:px-12 2xl:px-16">
+          <template v-for="goodsItem of goodsList">
+            <button v-if="tab === '全て' || goodsItem.tags.some((tag:any) => tag === tab)" @click="navigateTo(`/buyAndSell/${goodsItem.id}`)">
+              <ItemGoods :goodsItem="goodsItem" />
+            </button>
+          </template>
+        </ListGoods>
+      </swiper-slide>
+    </swiper-container>
+  </div>
 </template>
-
-
 
 <style scoped>
 .swiper-slide-thumb-active,
 .swiper-slide-thumb-active .line{
   opacity: 1!important;
 }
-
 </style>
